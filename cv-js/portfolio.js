e45d1a54ccbded93e0537174c8693971e2b6d5e0
Ahmed Mohammed <ahmed.mohd200130@gmail.com>
2022-01-27 03:47:48 +0200

    full responsive

--- cv-js/portfolio.js
+++
@@ -1,7 +0,0 @@
-let tools_icon = document.querySelector(".tools i"),
-ul_tools_items  = document.querySelector(".ul-tools-items");
-function show_tools() {
-  ul_tools_items.classList.toggle("show");
- 
-}
-tools_icon.onclick = show_tools;
